0797ba57b3eff8af0a03d1dd466c193cf5f16fb0
Aditya Bedare <adityabedare18@gmail.com>
2026-09-07 16:28:15 +0530

    Fix App routes and AdminOrders import

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,80 @@
-
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
-import Register from "./Myshop/Register";
+
 import Home from "./Myshop/Home";
 import Products from "./Myshop/Product";
 import ProductDetails from "./Myshop/ProductDetails";
 import Cart from "./Myshop/Cart";
 import Login from "./Myshop/Login";
+import Register from "./Myshop/Register";
 import Profile from "./Myshop/Profile";
-import AdminDashboard from "./admin/AdminDashboard";
-import AdminProducts from "./admin/AdminProducts";
 import Checkout from "./Myshop/Checkout";
 import Orders from "./Myshop/Orders";
-import AdminUsers from "./admin/AdminUsers";
-import ProtectedRoute from "./components/ProtectedRoute";
 import OrderDetails from "./Myshop/OrderDetails";
+
+import AdminDashboard from "./admin/AdminDashboard";
+import AdminProducts from "./admin/AdminProducts";
+import AdminOrders from "./admin/AdminOrders";
+import AdminUsers from "./admin/AdminUsers";
+
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   const [cartItems, setCartItems] = useState([]);
 
+  // Clear entire cart
   function clearCart() {
     setCartItems([]);
   }
 
-  // Add product
+  // Add product to cart
   function addToCart(product) {
     console.log("ADDING PRODUCT:", product._id, product.name);
 
-    const existingProduct = cartItems.find(
-      (item) => item._id === product._id
-    );
+    setCartItems((currentItems) => {
+      const existingProduct = currentItems.find(
+        (item) => item._id === product._id
+      );
 
-    if (existingProduct) {
-      setCartItems(
-        cartItems.map((item) =>
+      if (existingProduct) {
+        return currentItems.map((item) =>
           item._id === product._id
             ? {
-              ...item,
-              quantity: item.quantity + 1
-            }
+                ...item,
+                quantity: item.quantity + 1,
+              }
             : item
-        )
-      );
-    } else {
-      setCartItems([
-        ...cartItems,
+        );
+      }
+
+      return [
+        ...currentItems,
         {
           ...product,
-          quantity: 1
-        }
-      ]);
-    }
+          quantity: 1,
+        },
+      ];
+    });
   }
 
   // Remove product completely
   function removeFromCart(productId) {
-    setCartItems(
-      cartItems.filter(
-        (product) => product._id !== productId
-      )
+    setCartItems((currentItems) =>
+      currentItems.filter((product) => product._id !== productId)
     );
   }
 
   // Increase quantity
   function increaseQuantity(productId) {
-    setCartItems(
-      cartItems.map((product) =>
+    setCartItems((currentItems) =>
+      currentItems.map((product) =>
         product._id === productId
           ? {
-            ...product,
-            quantity: product.quantity + 1
-          }
+              ...product,
+              quantity: product.quantity + 1,
+            }
           : product
       )
     );
@@ -80,13 +82,13 @@ function App() {
 
   // Decrease quantity
   function decreaseQuantity(productId) {
-    setCartItems(
-      cartItems.map((product) =>
+    setCartItems((currentItems) =>
+      currentItems.map((product) =>
         product._id === productId && product.quantity > 1
           ? {
-            ...product,
-            quantity: product.quantity - 1
-          }
+              ...product,
+              quantity: product.quantity - 1,
+            }
           : product
       )
     );
@@ -94,30 +96,20 @@ function App() {
 
   return (
     <BrowserRouter>
-
       <Navbar />
 
       <Routes>
-
-        <Route
-          path="/"
-          element={<Home />}
-        />
+        {/* Public routes */}
+        <Route path="/" element={<Home />} />
 
         <Route
           path="/products"
-          element={
-            <Products addToCart={addToCart} />
-          }
+          element={<Products addToCart={addToCart} />}
         />
 
         <Route
           path="/products/:id"
-          element={
-            <ProductDetails
-              addToCart={addToCart}
-            />
-          }
+          element={<ProductDetails addToCart={addToCart} />}
         />
 
         <Route
@@ -132,26 +124,11 @@ function App() {
           }
         />
 
-        <Route
-          path="/login"
-          element={<Login />}
-        />
-        <Route
-          path="/register"
-          element={<Register />}
-        />
-        <Route
-          path="/Profile"
-          element={<Profile />}
-        />
-        <Route
-          path="/admin"
-          element={<AdminDashboard />}
-        />
-        <Route
-          path="/admin/products"
-          element={<AdminProducts />}
-        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/Profile" element={<Profile />} />
+
         <Route
           path="/checkout"
           element={
@@ -161,56 +138,21 @@ function App() {
             />
           }
         />
-        <Route
-          path="/orders"
-          element={<Orders />}
-        />
-        <Route
-          path="/admin/orders"
-          element={<AdminOrders />}
-        />
-        <Route
-          path="/admin/users"
-          element={<AdminUsers />}
-        />
+
+        {/* Protected user routes */}
         <Route element={<ProtectedRoute />}>
-          <Route
-            path="/orders"
-            element={<Orders />}
-          />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/orders/:id" element={<OrderDetails />} />
         </Route>
+
+        {/* Protected admin routes */}
         <Route element={<ProtectedRoute adminOnly />}>
-          <Route
-            path="/admin"
-            element={<AdminDashboard />}
-          />
-
-          <Route
-            path="/admin/products"
-            element={<AdminProducts />}
-          />
-
-          <Route
-            path="/admin/orders"
-            element={<AdminOrders />}
-          />
-
-          <Route
-            path="/admin/users"
-            element={<AdminUsers />}
-          />
-        </Route>
-        <Route
-          element={<ProtectedRoute />}
-        >
-          <Route
-            path="/orders/:id"
-            element={<OrderDetails />}
-          />
+          <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/admin/products" element={<AdminProducts />} />
+          <Route path="/admin/orders" element={<AdminOrders />} />
+          <Route path="/admin/users" element={<AdminUsers />} />
         </Route>
       </Routes>
-
-
     </BrowserRouter>
   );
 }
